be567c42c5b6f7b4ae8dbfaf1680259f4e26669a
Vitor Gomes <vitor1112pires@gmail.com>
2023-12-06 14:29:02 -0300

    atualização

---
+++ Javascripts/aula06/exe008.js
@@ -0,0 +1,7 @@
+var velocidade = 70
+console.log (`A velocidade do seu carro é ${velocidade} KM/h`)
+if(velocidade > 60){ // condição simples
+            
+    console.log(`Você ultrapassou a velocidade permitida, Multado!`)
+}
+console.log(`Dirija sempre usando cinto de segurança`)
