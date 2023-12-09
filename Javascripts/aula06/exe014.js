5afdc2e81eca5b722932ca20e618ce3bd705b687
Vitor Gomes <vitor1112pires@gmail.com>
2023-12-09 10:05:00 -0300

    atualizei

---
+++ Javascripts/aula06/exe014.js
@@ -0,0 +1,36 @@
+var contador = 1
+
+ do{
+    console.log (`Passo ${contador}`)
+    contador++
+
+ }while(contador <= 6 )
+   
+
+
+ /*
+ var contador = 1
+ 
+ while(contador <= 6 ){
+    
+   console.log (`Passo ${contador}`)
+    contador++
+ }
+
+
+
+
+
+
+
+
+/*
+console.log (' Tudo bem ?')
+console.log (' Tudo bem ?')
+console.log (' Tudo bem ?')
+console.log (' Tudo bem ?')
+console.log (' Tudo bem ?')
+console.log (' Tudo bem ?')
+console.log (' Tudo bem ?')
+console.log (' Tudo bem ?')
+*/
